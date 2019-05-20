a01069126e64284dbeed161c61187e7d9efa07a1
misodengaku <misodengaku@gmail.com>
2019-05-20 19:46:53 +0900

    release v0.1.1

--- SakuraIOTest/Properties/AssemblyInfo.cs
+++ SakuraIOTest/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SakuraIOTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sakura Internet Inc.")]
 [assembly: AssemblyProduct("SakuraIOTest")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Sakura Internet Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
